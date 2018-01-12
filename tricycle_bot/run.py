d72bf2aa41ce0338257332c35de562663d865184
Kavish Gandhi <kmbrgandhi@gmail.com>
2018-01-12 02:11:35 -0500

    Add temporary research queue, fix indentation errors.

--- tricycle_bot/run.py
+++ tricycle_bot/run.py
@@ -37,50 +37,50 @@ knight_to_cluster = {}
 # we should make this a global variable later
 building_queue = []
 current_worker_roles = {"miner":[],"builder":[],"blueprinter":[]}
+research.research_step(gc)
 
 while True:
     # We only support Python 3, which means brackets around print()
-	print('pyround:', gc.round())
-	try:
-		research.research_step(gc)
-		# walk through our units:
-		num_workers= num_knights=num_rangers= num_mages= num_healers= num_factory= num_rocket = 0
-		for unit in gc.my_units():
-			if unit.unit_type == bc.UnitType.Worker:
-				num_workers+=1
-			elif unit.unit_type == bc.UnitType.Knight:
-				num_knights+=1
-			elif unit.unit_type == bc.UnitType.Ranger:
-				num_rangers+=1
-			elif unit.unit_type == bc.UnitType.Mage:
-				num_mages+=1
-			elif unit.unit_type == bc.UnitType.Healer:
-				num_healers+=1
-			elif unit.unit_type == bc.UnitType.Factory:
-				num_factory+=1
-			elif unit.unit_type == bc.UnitType.Rocket:
-				num_rocket+=1
-		info = [num_workers, num_knights, num_rangers, num_mages, num_healers, num_factory, num_rocket]
-		for unit in gc.my_units():
-			# resepective unit types execute their own AI
-			if unit.unit_type == bc.UnitType.Worker:
-				worker.timestep(gc,unit,building_queue,current_worker_roles)
-			elif unit.unit_type == bc.UnitType.Knight:
-				knight.timestep(gc,unit,info,knight_to_cluster)
-			elif unit.unit_type == bc.UnitType.Ranger:
-				ranger.timestep(gc,unit,info)
-			elif unit.unit_type == bc.UnitType.Mage:
-				mage.timestep(gc,unit,info)
-			elif unit.unit_type == bc.UnitType.Healer:
-				healer.timestep(gc,unit,info)
-			elif unit.unit_type == bc.UnitType.Factory:
-				factory.timestep(gc,unit,info)
-			elif unit.unit_type == bc.UnitType.Rocket:
-				rocket.timestep(gc,unit,info)
-	except Exception as e:
-		print('Error:', e)
-		# use this to show where the error was
-		traceback.print_exc()
+    print('pyround:', gc.round())
+    try:
+        # walk through our units:
+        num_workers= num_knights=num_rangers= num_mages= num_healers= num_factory= num_rocket = 0
+        for unit in gc.my_units():
+            if unit.unit_type == bc.UnitType.Worker:
+                num_workers+=1
+            elif unit.unit_type == bc.UnitType.Knight:
+                num_knights+=1
+            elif unit.unit_type == bc.UnitType.Ranger:
+                num_rangers+=1
+            elif unit.unit_type == bc.UnitType.Mage:
+                num_mages+=1
+            elif unit.unit_type == bc.UnitType.Healer:
+                num_healers+=1
+            elif unit.unit_type == bc.UnitType.Factory:
+                num_factory+=1
+            elif unit.unit_type == bc.UnitType.Rocket:
+                num_rocket+=1
+        info = [num_workers, num_knights, num_rangers, num_mages, num_healers, num_factory, num_rocket]
+        for unit in gc.my_units():
+            # resepective unit types execute their own AI
+            if unit.unit_type == bc.UnitType.Worker:
+                worker.timestep(gc,unit,building_queue,current_worker_roles)
+            elif unit.unit_type == bc.UnitType.Knight:
+                knight.timestep(gc,unit,info,knight_to_cluster)
+            elif unit.unit_type == bc.UnitType.Ranger:
+                ranger.timestep(gc,unit,info)
+            elif unit.unit_type == bc.UnitType.Mage:
+                mage.timestep(gc,unit,info)
+            elif unit.unit_type == bc.UnitType.Healer:
+                healer.timestep(gc,unit,info)
+            elif unit.unit_type == bc.UnitType.Factory:
+                factory.timestep(gc,unit,info)
+            elif unit.unit_type == bc.UnitType.Rocket:
+                rocket.timestep(gc,unit,info)
+    except Exception as e:
+        print('Error:', e)
+        # use this to show where the error was
+        traceback.print_exc()
 
     # send the actions we've performed, and wait for our next turn.
     gc.next_turn()
@@ -104,7 +104,7 @@ class Knight_Cluster():
         if knight_id in self.knight_ids:
             self.knight_ids.remove(knight_id)
 
-    def set_target_loc(self, loc): 
+    def set_target_loc(self, loc):
         self.target_loc = loc
 
     def set_target_unit(self, unit):
