edc881f17174b01447d0c100b62b62a350a6650f
Kavish Gandhi <kmbrgandhi@gmail.com>
2018-01-12 01:27:44 -0500

    Compute optimal durations

--- tricycle_bot/Units/rocket.py
+++ tricycle_bot/Units/rocket.py
@@ -10,3 +10,11 @@ def timestep(gc, unit,composition):
 		# prob should return some kind of error
 		return
 
+
+def rocket_durations_sorted(gc):
+	# gets orbit pattern, sorts round numbers by duration time, returns the resultant array.
+	orbit_pattern = gc.orbit_pattern()
+	rounds = [i for i in range(1, 1001)]
+	durations = [(i, orbit_pattern.duration(i)) for i in range(1, 1001)]
+	sorted_rounds_by_duration = sorted(rounds, key = lambda x: orbit_pattern.duration(x))
+	return sorted_rounds_by_duration
