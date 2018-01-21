2dce1c81fa12e0b9508f5236421bf77722198dc7
kmbrgandhi <kmbrgandhi@gmail.com>
2018-01-20 22:44:07 -0500

    Undo optimizations :'(

--- tricycle_bot/Units/Ranger.py
+++ tricycle_bot/Units/Ranger.py
@@ -48,7 +48,7 @@ def timestep(unit):
                 next_turn_battle_locs[f_f_quad] = (next_turn_battle_locs[f_f_quad][0], next_turn_battle_locs[f_f_quad][1]+1)
 
         if move_then_attack:
-            if dir!=None and gc.is_move_ready(unit.id):
+            if dir!=None and gc.is_move_ready(unit.id) and gc.can_move(unit.id, dir):
                 gc.move_robot(unit.id, dir)
 
             if attack_target is not None and gc.is_attack_ready(unit.id) and gc.can_attack(unit.id, attack_target.id):
@@ -65,7 +65,7 @@ def timestep(unit):
                     targeting_units[attack_target.id]+=1
                 gc.attack(unit.id, attack_target.id)
 
-            if dir != None and gc.is_move_ready(unit.id):
+            if dir != None and gc.is_move_ready(unit.id) and gc.can_move(unit.id, dir):
                 gc.move_robot(unit.id, dir)
 
         if snipe!=None and gc.can_begin_snipe(unit.id, snipe.location.map_location()) and gc.is_begin_snipe_ready(unit.id):
