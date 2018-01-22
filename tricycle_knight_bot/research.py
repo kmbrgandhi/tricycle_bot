b5a56703241819804531809b254312a2f29518e8
chinitanao <isanaoqui@gmail.com>
2018-01-22 00:16:23 -0500

    adding our previous knight bot

---
+++ tricycle_knight_bot/research.py
@@ -0,0 +1,17 @@
+import battlecode as bc
+import random
+import sys
+import traceback
+
+def research_step(gc):
+    gc.queue_research(bc.UnitType.Worker)
+    gc.queue_research(bc.UnitType.Ranger)
+    gc.queue_research(bc.UnitType.Rocket)
+    gc.queue_research(bc.UnitType.Ranger)
+    gc.queue_research(bc.UnitType.Ranger)
+    gc.queue_research(bc.UnitType.Worker)
+    gc.queue_research(bc.UnitType.Worker)
+    gc.queue_research(bc.UnitType.Worker)
+    gc.queue_research(bc.UnitType.Rocket)
+    gc.queue_research(bc.UnitType.Rocket)
+
