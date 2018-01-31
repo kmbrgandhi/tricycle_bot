1c1de38d20a392c6322fabd8b90a5f378d572281
kmbrgandhi <kmbrgandhi@gmail.com>
2018-01-31 17:46:02 -0500

    some changes.

--- tricycle_bot/Units/variables.py
+++ tricycle_bot/Units/variables.py
@@ -297,7 +297,7 @@ if curr_planet == bc.Planet.Earth:
             if dist!=float('inf'):
                 dists.append(dist)
     if len(dists)>0:
-        if min(dists) < 25 and max(dists) < 33:
+        if min(dists) < 22 and max(dists) < 30:
             knight_rush = True
 
     ## Karbonite locations update
